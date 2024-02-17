0e3f7e539a7df4b33bf15ef478c012639f561233
waku_25 <yado.kt@me.com>
2024-02-17 22:07:47 +0900

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/main.scss'
 import React from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  )
 }
 
 export default MyApp
